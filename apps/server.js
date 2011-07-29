119c92aa90dae5c3635642d57339b3b778c955c9
Yusuf Simonson <simonson@gmail.com>
2011-07-29 17:21:32 -0400

    Removed unnecessary argument for initializing node-bus on the server

--- apps/server.js
+++ apps/server.js
@@ -6,11 +6,9 @@ var url = require('url');
 var sys = require('sys');
 var bus = require('./lib/node-bus/bus.server');
 
-var NODEBUS_PATTERN = /\/nodebus/;
-
 var dirHandler = router.staticDirHandler("static/", "", ["index.html"]);
 var httpServer = http.createServer();
-var busServer = new bus.BusServer(httpServer, NODEBUS_PATTERN);
+var busServer = new bus.BusServer(httpServer);
 
 httpServer.addListener('request', function (req, res) {
     if(req.method === "GET" && !NODEBUS_PATTERN.test(req.url)) {
