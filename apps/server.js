51245e0159fcf2072e35b10e85a489e9fa06e843
Yusuf Simonson <simonson@gmail.com>
2011-07-29 17:24:02 -0400

    Added back pattern constant

--- apps/server.js
+++ apps/server.js
@@ -6,6 +6,8 @@ var url = require('url');
 var sys = require('sys');
 var bus = require('./lib/node-bus/bus.server');
 
+var NODEBUS_PATTERN = /\/nodebus/;
+
 var dirHandler = router.staticDirHandler("static/", "", ["index.html"]);
 var httpServer = http.createServer();
 var busServer = new bus.BusServer(httpServer);
